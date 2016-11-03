b854740021c60ed78c5cf5283a8d4154e06fcb13
rizwanahmed19 <rizwan.ahmed19@outlook.com>
2016-11-03 16:44:47 +0500

    fix bugs in Examples page

--- public/bundle.js
+++ public/bundle.js
@@ -68,7 +68,7 @@
 
 	var _About2 = _interopRequireDefault(_About);
 
-	var _Examples = __webpack_require__(!(function webpackMissingModule() { var e = new Error("Cannot find module \"./components/Examples\""); e.code = 'MODULE_NOT_FOUND'; throw e; }()));
+	var _Examples = __webpack_require__(260);
 
 	var _Examples2 = _interopRequireDefault(_Examples);
 
@@ -28283,6 +28283,41 @@
 	};
 
 	exports.default = About;
+
+/***/ },
+/* 260 */
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+
+	Object.defineProperty(exports, "__esModule", {
+		value: true
+	});
+
+	var _react = __webpack_require__(1);
+
+	var _react2 = _interopRequireDefault(_react);
+
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+	var Examples = function Examples(props) {
+		return _react2.default.createElement(
+			'div',
+			null,
+			_react2.default.createElement(
+				'h2',
+				null,
+				'Examples'
+			),
+			_react2.default.createElement(
+				'p',
+				null,
+				'Welcome to examples page!'
+			)
+		);
+	};
+
+	exports.default = Examples;
 
 /***/ }
 /******/ ]);
--- src/components/Examples.js
+++ src/components/Examples.js
@@ -2,8 +2,10 @@ import React, {Component} from 'react';
 
 var Examples = props => {
 	return (
-			<h2>Examples</h2>
-			<p>Welcome to examples page!</p>
+			<div>
+				<h2>Examples</h2>
+				<p>Welcome to examples page!</p>
+			</div>
 	);
 }
 
