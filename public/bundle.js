cab83e0f23e2af626e9539841e3511121e023c5b
rizwanahmed19 <rizwan.ahmed19@outlook.com>
2016-11-06 15:19:55 +0500

    fix modal error

--- public/bundle.js
+++ public/bundle.js
@@ -28452,6 +28452,14 @@
 
 	var _react2 = _interopRequireDefault(_react);
 
+	var _reactDom = __webpack_require__(40);
+
+	var _reactDom2 = _interopRequireDefault(_reactDom);
+
+	var _server = __webpack_require__(274);
+
+	var _server2 = _interopRequireDefault(_server);
+
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
@@ -28472,17 +28480,11 @@
 		_createClass(ErrorModal, [{
 			key: 'componentDidMount',
 			value: function componentDidMount() {
-				var modal = new Foundation.Reveal($('#error-modal'));
-				modal.open();
-			}
-		}, {
-			key: 'render',
-			value: function render() {
 				var _props = this.props;
 				var title = _props.title;
 				var message = _props.message;
 
-				return _react2.default.createElement(
+				var modalMarkup = _react2.default.createElement(
 					'div',
 					{ id: 'error-modal', className: 'reveal tiny text-center', 'data-reveal': '' },
 					_react2.default.createElement(
@@ -28505,6 +28507,17 @@
 						)
 					)
 				);
+
+				var $modal = $(_server2.default.renderToString(modalMarkup));
+				$(_reactDom2.default.findDOMNode(this)).html($modal);
+
+				var modal = new Foundation.Reveal($('#error-modal'));
+				modal.open();
+			}
+		}, {
+			key: 'render',
+			value: function render() {
+				return _react2.default.createElement('div', null);
 			}
 		}]);
 
@@ -29032,6 +29045,169 @@
 
 	// exports
 
+
+/***/ },
+/* 274 */
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+
+	module.exports = __webpack_require__(275);
+
+
+/***/ },
+/* 275 */
+/***/ function(module, exports, __webpack_require__) {
+
+	/**
+	 * Copyright 2013-present, Facebook, Inc.
+	 * All rights reserved.
+	 *
+	 * This source code is licensed under the BSD-style license found in the
+	 * LICENSE file in the root directory of this source tree. An additional grant
+	 * of patent rights can be found in the PATENTS file in the same directory.
+	 *
+	 * @providesModule ReactDOMServer
+	 */
+
+	'use strict';
+
+	var ReactDefaultInjection = __webpack_require__(45);
+	var ReactServerRendering = __webpack_require__(276);
+	var ReactVersion = __webpack_require__(38);
+
+	ReactDefaultInjection.inject();
+
+	var ReactDOMServer = {
+	  renderToString: ReactServerRendering.renderToString,
+	  renderToStaticMarkup: ReactServerRendering.renderToStaticMarkup,
+	  version: ReactVersion
+	};
+
+	module.exports = ReactDOMServer;
+
+/***/ },
+/* 276 */
+/***/ function(module, exports, __webpack_require__) {
+
+	/* WEBPACK VAR INJECTION */(function(process) {/**
+	 * Copyright 2013-present, Facebook, Inc.
+	 * All rights reserved.
+	 *
+	 * This source code is licensed under the BSD-style license found in the
+	 * LICENSE file in the root directory of this source tree. An additional grant
+	 * of patent rights can be found in the PATENTS file in the same directory.
+	 *
+	 * @providesModule ReactServerRendering
+	 */
+	'use strict';
+
+	var _prodInvariant = __webpack_require__(13);
+
+	var ReactDOMContainerInfo = __webpack_require__(169);
+	var ReactDefaultBatchingStrategy = __webpack_require__(142);
+	var ReactElement = __webpack_require__(15);
+	var ReactInstrumentation = __webpack_require__(68);
+	var ReactMarkupChecksum = __webpack_require__(171);
+	var ReactReconciler = __webpack_require__(65);
+	var ReactServerBatchingStrategy = __webpack_require__(277);
+	var ReactServerRenderingTransaction = __webpack_require__(135);
+	var ReactUpdates = __webpack_require__(62);
+
+	var emptyObject = __webpack_require__(25);
+	var instantiateReactComponent = __webpack_require__(127);
+	var invariant = __webpack_require__(14);
+
+	var pendingTransactions = 0;
+
+	/**
+	 * @param {ReactElement} element
+	 * @return {string} the HTML markup
+	 */
+	function renderToStringImpl(element, makeStaticMarkup) {
+	  var transaction;
+	  try {
+	    ReactUpdates.injection.injectBatchingStrategy(ReactServerBatchingStrategy);
+
+	    transaction = ReactServerRenderingTransaction.getPooled(makeStaticMarkup);
+
+	    pendingTransactions++;
+
+	    return transaction.perform(function () {
+	      var componentInstance = instantiateReactComponent(element, true);
+	      var markup = ReactReconciler.mountComponent(componentInstance, transaction, null, ReactDOMContainerInfo(), emptyObject, 0 /* parentDebugID */
+	      );
+	      if (process.env.NODE_ENV !== 'production') {
+	        ReactInstrumentation.debugTool.onUnmountComponent(componentInstance._debugID);
+	      }
+	      if (!makeStaticMarkup) {
+	        markup = ReactMarkupChecksum.addChecksumToMarkup(markup);
+	      }
+	      return markup;
+	    }, null);
+	  } finally {
+	    pendingTransactions--;
+	    ReactServerRenderingTransaction.release(transaction);
+	    // Revert to the DOM batching strategy since these two renderers
+	    // currently share these stateful modules.
+	    if (!pendingTransactions) {
+	      ReactUpdates.injection.injectBatchingStrategy(ReactDefaultBatchingStrategy);
+	    }
+	  }
+	}
+
+	/**
+	 * Render a ReactElement to its initial HTML. This should only be used on the
+	 * server.
+	 * See https://facebook.github.io/react/docs/top-level-api.html#reactdomserver.rendertostring
+	 */
+	function renderToString(element) {
+	  !ReactElement.isValidElement(element) ? process.env.NODE_ENV !== 'production' ? invariant(false, 'renderToString(): You must pass a valid ReactElement.') : _prodInvariant('46') : void 0;
+	  return renderToStringImpl(element, false);
+	}
+
+	/**
+	 * Similar to renderToString, except this doesn't create extra DOM attributes
+	 * such as data-react-id that React uses internally.
+	 * See https://facebook.github.io/react/docs/top-level-api.html#reactdomserver.rendertostaticmarkup
+	 */
+	function renderToStaticMarkup(element) {
+	  !ReactElement.isValidElement(element) ? process.env.NODE_ENV !== 'production' ? invariant(false, 'renderToStaticMarkup(): You must pass a valid ReactElement.') : _prodInvariant('47') : void 0;
+	  return renderToStringImpl(element, true);
+	}
+
+	module.exports = {
+	  renderToString: renderToString,
+	  renderToStaticMarkup: renderToStaticMarkup
+	};
+	/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(9)))
+
+/***/ },
+/* 277 */
+/***/ function(module, exports) {
+
+	/**
+	 * Copyright 2014-present, Facebook, Inc.
+	 * All rights reserved.
+	 *
+	 * This source code is licensed under the BSD-style license found in the
+	 * LICENSE file in the root directory of this source tree. An additional grant
+	 * of patent rights can be found in the PATENTS file in the same directory.
+	 *
+	 * @providesModule ReactServerBatchingStrategy
+	 */
+
+	'use strict';
+
+	var ReactServerBatchingStrategy = {
+	  isBatchingUpdates: false,
+	  batchedUpdates: function (callback) {
+	    // Don't do anything here. During the server rendering we don't want to
+	    // schedule any updates. We will simply ignore them.
+	  }
+	};
+
+	module.exports = ReactServerBatchingStrategy;
 
 /***/ }
 /******/ ]);
--- src/components/ErrorModal.js
+++ src/components/ErrorModal.js
@@ -1,19 +1,29 @@
 import React, {Component, PropTypes} from 'react';
+import ReactDOM from 'react-dom';
+import ReactDOMServer from 'react-dom/server';
 
 class ErrorModal extends Component {
 	componentDidMount(){
-		var modal = new Foundation.Reveal($('#error-modal'));
-		modal.open();
-	}
-	render(){
 		var {title, message} = this.props;
-		return (
+		var modalMarkup = (
 			<div id='error-modal' className='reveal tiny text-center' data-reveal="">
 				<h4>{title}</h4>
 				<p>{message}</p>
 				<p>
 					<button className='button hollow' data-close="">Okay!</button>
 				</p>
+			</div>
+		);
+
+		var $modal = $(ReactDOMServer.renderToString(modalMarkup));
+		$(ReactDOM.findDOMNode(this)).html($modal);
+
+		var modal = new Foundation.Reveal($('#error-modal'));
+		modal.open();
+	}
+	render(){
+		return (
+			<div>
 			</div>
 		);
 	}
